3013bf410876c1909b32cd943253e8ffa2dea16d
ueberte1 <94020328+ueberte1@users.noreply.github.com>
2024-05-31 14:38:06 -0300

    Create mov.asm

---
+++ mov.asm
@@ -0,0 +1,35 @@
+; move .asm
+section .data
+    bNum    db  123
+    wNum    dw  12345
+    dNum    dd  1234567890
+    qNum1   dq  1234567890123456789
+    qNum2   dq  123456
+    qNum3   dq  3.14
+section .bss
+section .text
+    global main
+main:
+    push    rbp
+    mov     rbp,rsp
+    mov     rax,-1  ;fill rax with 1s
+    mov     al, byte [bNum] ;does NOT clear upper bits of rax
+    xor     rax, rax    ;clear the rax
+    mov     al, byte [bNum] ;now rax has the correct value
+    
+    mov     rax,-1  ;fill rax with 1s
+    mov     ax, word [wNum] 
+    xor     rax,rax     ;clear rax
+    mov     ax, word[wNum]  ;now rax has the correct value
+    
+    mov     rax,-1      ;fill rax with 1s
+    mov     rax, qword[qNum1]      ;does clear upper bits of rax
+    mov     qword[qNum2], rax   ;one opearand always a register
+    mov     rax, 123456         ;source operand an immediate value
+    
+    movsd     xmm0, [qNum3]       ;instruction for floating point
+    
+    mov    rsp,rbp
+    pop     rbp
+    
+    ret
